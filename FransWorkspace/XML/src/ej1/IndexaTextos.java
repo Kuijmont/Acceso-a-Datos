bf7cd5eab233770539406faca517d09f23023804
Unknown <fkmontero9@gmail.com>
2018-10-23 14:23:46 +0200

    XML

--- FransWorkspace/XML/src/ej1/IndexaTextos.java
+++ FransWorkspace/XML/src/ej1/IndexaTextos.java
@@ -1,10 +1,12 @@
 package ej1;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -16,98 +18,90 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Comment;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
-import org.xml.sax.SAXException;
 
 public class IndexaTextos {
 
 	public static void main(String[] args) throws DOMException, IOException {
 		try {
 			String ruta = "/home/alumno/biblioteca";
-			
-			ArrayList <String> al = sacarRutas(ruta);
-			ArrayList <String> nombre = new ArrayList<String>();
-			String file,rut = null;
+
+			ArrayList<String> al = sacarRutas(ruta);
+			ArrayList<String> nombre = new ArrayList<String>();
+			String file, rut = null;
 			for (int i = 0; i < al.size(); i++) {
 				rut = al.get(i).toString();
-				file =rut.substring(rut.lastIndexOf('/') + 1);
+				file = rut.substring(rut.lastIndexOf('/') + 1);
 				nombre.add(file);
 			}
-				
-			
-			
-			//Creamos arbol DOM
-	        DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
-	        DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+
+			// Creamos arbol DOM
+			DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
 			Document doc = docBuilder.newDocument();
-			
-			//crea elemento raiz
-            Element raiz = doc.createElement("biblioteca");
-            doc.appendChild(raiz);
-            
-	         for (int i = 0; i < al.size(); i++) {	
-	        	BufferedReader bfr = new BufferedReader(new FileReader(new File(al.get(i).toString())));
-	            //crea elemento hijo, aniade atributo, y lo cuelga de raiz
-	            Element padre = doc.createElement("libro");
-	            padre.setAttribute("titulo", nombre.get(i));
-	            raiz.appendChild(padre);   
-	            
-	            Element hijo = doc.createElement("linea");
-	            String linea;
-	            int cnt=0;
-	            while((linea=bfr.readLine())!=null) {
-	            	
-	            	cnt++;
-	            }
-	            bfr.close();
-	            for (int j = 0; j < cnt+1; j++) {
-	            	hijo.setAttribute("num",""+j);
-	            	
+
+			// crea elemento raiz
+			Element raiz = doc.createElement("biblioteca");
+			doc.appendChild(raiz);
+
+			for (int i = 0; i < al.size(); i++) {
+				int cnt = 0;
+				//BufferedReader bfr = new BufferedReader(new FileReader(new File(al.get(i).toString())));
+				//DataInputStream dis = new DataInputStream(new FileInputStream(new File(al.get(i).toString())));
+				RandomAccessFile fichero = new RandomAccessFile(al.get(i).toString(), "rw");
+				// crea elemento hijo, aniade atributo, y lo cuelga de raiz
+				Element padre = doc.createElement("libro");
+				padre.setAttribute("titulo", nombre.get(i));
+				raiz.appendChild(padre);
+				String linea;
+				String pos = String.valueOf(fichero.getFilePointer());
+				while ((linea = fichero.readLine()) != null) {
+					
+					Element hijo = doc.createElement("linea");
+					hijo.setTextContent(pos);
+					cnt++;
+
+					for (int j = 0; j < cnt + 1; j++) {
+						hijo.setAttribute("num", "" + j);
+					}
+					
+					pos = String.valueOf(fichero.getFilePointer());
+					padre.appendChild(hijo);
+
 				}
-	            hijo.setTextContent(linea);
-	            padre.appendChild(hijo);
-	            cnt=0;
-	         }
-	         
+				fichero.close();
+			}
+
 			// Escribe arbol DOM a fichero XML
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
 			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
 			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("nuevo.xml"));
+			StreamResult result = new StreamResult(new File("indice.xml"));
 			transformer.transform(source, result);
-			
-			
+
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
+
 	}
 
-	private static ArrayList <String> sacarRutas(String ruta) {
+	private static ArrayList<String> sacarRutas(String ruta) {
 		File fil = new File(ruta);
-		ArrayList <String>al = new ArrayList<String>();
+		ArrayList<String> al = new ArrayList<String>();
 		File listFile[] = fil.listFiles();
-        if (listFile != null) {
-            for (int i = 0; i < listFile.length; i++) {
-                    al.add(listFile[i].getPath());
-            }
-        }
-        
+		if (listFile != null) {
+			for (int i = 0; i < listFile.length; i++) {
+				al.add(listFile[i].getPath());
+			}
+		}
+
 		return al;
 	}
 
