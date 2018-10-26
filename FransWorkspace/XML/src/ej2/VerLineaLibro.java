68efc7d5accc200fa6c7f7876f657c3bdeb66030
Unknown <fkmontero9@gmail.com>
2018-10-26 14:19:05 +0200

    xml 1.5

--- FransWorkspace/XML/src/ej2/VerLineaLibro.java
+++ FransWorkspace/XML/src/ej2/VerLineaLibro.java
@@ -3,14 +3,20 @@ package ej2;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -20,11 +26,12 @@ public class VerLineaLibro{
 	public static void main(String[] args){
 		try {
 			boolean tituloExiste = false;
+			String titulo,ruta;
 			do {
 				System.out.print("Introduzca título: ");
-				String titulo = Entrada.cadena();
+				titulo = Entrada.cadena();
 				
-				String ruta = "/home/alumno/biblioteca";
+				ruta = "/home/alumno/biblioteca";
 	
 				ArrayList<String> al = sacarRutas(ruta);
 				ArrayList<String> nombre = new ArrayList<String>();
@@ -44,19 +51,38 @@ public class VerLineaLibro{
 			System.out.print("Introduzca línea: ");
 			int linea = Entrada.entero();
 			
-			
-
+			RandomAccessFile fichero = new RandomAccessFile(ruta+"/"+titulo, "rw");
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 			Document doc = docBuilder.parse(new InputSource(new FileInputStream("indice.xml"))); //Crea erbol DOM a partir de archivo XML	
 
-		    Element elementRaiz = doc.getDocumentElement();  
-	
-		    
-	    
-	
+            doc.getDocumentElement().normalize();
+            //System.out.println("Elemento raiz:" + doc.getDocumentElement().getNodeName());
+            NodeList libro = doc.getElementsByTagName("libro");
 		
-		
+            for (int temp = 0; temp < libro.getLength(); temp++) {
+                Node nodo = libro.item(temp);
+
+                if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) nodo;
+                    //System.out.println("Titulo: " + element.getAttribute("titulo"));
+                    for (int i = 0; i < element.getElementsByTagName("linea").getLength(); i++) {
+						if(i+1==linea) {
+							
+							String ele=element.getElementsByTagName("linea").item(i).getTextContent();
+							long la = Long.valueOf(ele);
+							System.out.println(la);
+							fichero.seek(la);
+							System.out.println(fichero.readLine());
+							
+						}
+					}
+                    
+                    //System.out.println("linea: " + element.getElementsByTagName("linea").item(1).getTextContent());
+                }break;
+            }
+            fichero.close();
+            
 		} catch (ParserConfigurationException pce) {
 			pce.printStackTrace();
 		} catch (SAXException e) {
